2dea111ccdb45ebeed3b1d100051f0c1da85a4d3
Samuel Chandler <sec180005@utdallas.edu>
2023-05-18 11:43:47 -0500

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+// Package Import
+const express = require('express');
+const path = require('path');
+const fileupload = require('express-fileupload');
+
+// Set initial path
+let initial_path = path.join(__dirname, "public");
+
+// Set static path and enable file upload
+const app = express();
+app.use(express.static(initial_path));
+app.use(fileupload());
+
+// send home html and use port 3000(used by default by http servers)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initial_path, "home.html"));
+})
+
+app.listen("3000", () => {
+    console.log('listening......');
+})
+
