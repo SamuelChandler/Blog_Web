69f345ffa3fa1c6ff87629862f025e5d3eb4980d
Samuel Chandler <sec180005@utdallas.edu>
2023-05-25 14:55:31 -0500

    might be fixed

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,11 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
+
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+  let firebaseConfig = {
     apiKey: "AIzaSyCZDeIdPm0LBXiwsTTRh0KqrdYBr--JwJ0",
     authDomain: "blog-sam-c.firebaseapp.com",
     projectId: "blog-sam-c",
@@ -14,9 +13,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebas
     appId: "1:748766726265:web:625b588025018cecf7aca8",
     measurementId: "G-GNTWVKFJWP"
   };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+  
+  firebase.initializeApp(firebaseConfig);
 
   let db = firebase.firestore();
